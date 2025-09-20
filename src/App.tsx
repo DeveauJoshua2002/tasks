ae8f17916eba202d3856f5607643b913bd83934a
Joshua Deveau <edward.deveau@maine.edu>
2025-09-20 14:13:29 -0400

    Added list with three elements

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,17 @@ function App(): React.JSX.Element {
             <header className="App-header">
                 UM COS420 with React Hooks and TypeScript
             </header>
-            <h1>This is a test header</h1>
+            
+            <h1>This is a header</h1>
+
+            <img src="" />
+            
+            <ul>
+                <li>First list</li>
+                <li>Second list</li>
+                <li>Third list</li>
+            </ul>
+
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Hello World. This is Joshua Deveau
